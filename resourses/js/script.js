e19fc5e51e5c8fa5992f246fe90daabde24eb395
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-01-28 09:47:27 +0500

    1.4

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,14 +1,44 @@
 "use strict;"
 
-const panels = document.querySelectorAll(".panels .panel");
+// const panels = document.querySelectorAll(".panels .panel");
 
-function openPanel(e) {
-    for(const item of this.parentNode.children){
-        item.classList.remove("open");
-    }
-    this.classList.add("open");
+// function openPanel(e) {
+//     for(const item of this.parentNode.children){
+//         item.classList.remove("open");
+//     }
+//     this.classList.add("open");
+// }
+
+// for (const panel of panels) {
+//     panel.addEventListener('click', openPanel);
+// }
+
+// const panels = document.querySelectorAll(".panel");
+
+
+
+
+
+// // biome-ignore lint/complexity/noForEach: <explanation>
+// panels.forEach(panel => panel.addEventListener('click',toggleOpen));
+
+const panels = document.querySelectorAll(".panel");
+
+// biome-ignore lint/complexity/noForEach: <explanation>
+
+function toggleOpen(){
+    panels.forEach(sib => sib.classList.remove("open"));
+    this.classList.toggle("open");
 }
 
-for (const panel of panels) {
-    panel.addEventListener('click', openPanel);
+function animateText(e){
+    //e.propagate.stop;
+    if(e.propertyName.includes("font")){
+        this.classList.toggle('textAnimate');
+    }    
 }
+
+// biome-ignore lint/complexity/noForEach: <explanation>
+panels.forEach(panel => panel.addEventListener('click',toggleOpen));
+// biome-ignore lint/complexity/noForEach: <explanation>
+panels.forEach(panel => panel.addEventListener('transitionend',animateText));
